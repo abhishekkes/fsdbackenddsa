96af605e872954f3298baa83a9e749def6df398c
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-03-28 13:02:25 +0530

    User Registration System

--- Day5/frontend/src/App.jsx
+++ Day5/frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Register from './components/Register'
+import View from './components/View'
+import Update from './components/Update'
+import Delete from './components/Delete'
 
 const App = () => {
   return (
     <div>
-      <h1>User Registration System</h1>
+      <h1 style={{backgroundColor: 'greenyellow'}}>User Registration System</h1>
       <Register/>
+      <Update/>
+      <Delete/>
+      <View/>
     </div>
   )
 }
