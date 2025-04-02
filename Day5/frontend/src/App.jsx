0526eb75c8d7925a999942f5a2ebb01cd5642f39
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-04-02 11:29:37 +0530

    Day5

--- Day5/frontend/src/App.jsx
+++ Day5/frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Register from './components/Register'
 import View from './components/View'
 import Update from './components/Update'
 import Delete from './components/Delete'
-
+import './App.css'
 const App = () => {
   return (
     <div>
